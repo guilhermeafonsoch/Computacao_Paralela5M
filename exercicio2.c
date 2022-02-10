c50fa1fbbb66fd4156b87be080821af43f64053c
guilhermeafonsoch <ch.afonso.gui@gmail.com>
2022-02-10 20:41:56 -0300

    Inicios dois programs

---
+++ exercicio2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/*
+
+* NOME: GUILHERME AFONSO CHRISTINO
+
+* TIA: 32030657
+
+* 2) Escreva um programa que leia 3 notas de um aluno e a média das notas dos exercícios realizados por ele.
+     Calcular a média de aproveitamento, usando a fórmula: MA = (N1 + N2*2 + N3*3 + ME)/7. A partir da média, informar o conceito de acordo com a tabela:
+     maior ou igual a 9	A
+     maior ou igual a 7.5 e menor que 9	B
+     maior ou igual a 6 e menor que 7.5	C
+     maior ou igual a 4 e menor que 6	D
+     menor que 4	E
+*/
+
+
+
+int main()
+{
+    printf("Hello world2!\n");
+    return 0;
+}
+
---
+++ exercicio8.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/*
+
+* NOME: GUILHERME AFONSO CHRISTINO
+
+* TIA: 32030657
+
+* 8) Crie uma função capaz de criar a transposta de uma matriz.
+
+*/
+
+
+
+int main()
+{
+    printf("Hello world8!\n");
+    return 0;
+}
+
